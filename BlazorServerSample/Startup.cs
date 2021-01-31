aaff1fd7a28e2472a8fd3a436b52e21a81ca5846
Mika Berglund <mika.berglund@meteoriitti.com>
2021-01-31 11:43:13 +0200

    Added sample apps for both Blazor Server and Blazor WASM.

--- BlazorServerSample/Startup.cs
+++ BlazorServerSample/Startup.cs
@@ -11,7 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ServerTestApp
+namespace BlazorServerSample
 {
     public class Startup
     {
--- BlazorWasmSample/Program.cs
+++ BlazorWasmSample/Program.cs
@@ -9,7 +9,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WasmTestApp
+namespace BlazorWasmSample
 {
     public class Program
     {
