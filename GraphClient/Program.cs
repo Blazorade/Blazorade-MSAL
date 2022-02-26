05ad49009c387ea6199fc2d6924713065f90d1c9
Mika Berglund <mika.berglund@outlook.com>
2022-02-26 22:40:09 +0200

    Modified the sample app to acquire token for User.Read and Files.Read.

--- GraphClient/Program.cs
+++ GraphClient/Program.cs
@@ -6,6 +6,14 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services
+    .AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+    .AddBlazoradeMsal((sp, options) =>
+    {
+        var root = sp.GetService<IConfiguration>() ?? throw new Exception($"Cannot get service instance for {typeof(IConfiguration).FullName}.");
+        var config = root.GetSection("app");
+        config.Bind(options);
+    })
+    ;
 
 await builder.Build().RunAsync();
